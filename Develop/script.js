0be35260dd783994595d5548f4ee88660e677af1
Isaac Severing <I.N.Severing@gmail.com>
2022-01-28 20:09:25 -0500

    I have fleshed out the README.md file and have provided comments in the javascript to allow other developers to understand why each function and variable is imortant.

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,4 @@
-// Assignment Code
+// defining all variables i'd like to be globaly scoped
 var generateBtn = document.querySelector("#generate");
 var passwordLength = undefined;
 var uppercase = undefined;
@@ -6,8 +6,7 @@ var lowercase = undefined;
 var numeric = undefined;
 var specialCharacters = undefined;
 
-// Write password to the #password input
-
+// making function for user to input desired length of password, as well as logic for prompts and redirection if the value they input isn't a number
  function validLength() {
   passwordLength = parseInt(prompt("How long would you like your password to be? it must be between 8 and 128 characters. (please enter a number value)",))
    if (8>passwordLength||passwordLength>128||isNaN(passwordLength)){
@@ -19,7 +18,7 @@ var specialCharacters = undefined;
    }
    return;
  };
-
+// making function for confirmation if the user wants lowercases, returns boolean value as the new value of variable.
  function lowercasePrompt() {
    lowercase = confirm("include lowercase letters?");
    if (lowercase===true){
@@ -29,7 +28,7 @@ var specialCharacters = undefined;
    };
    return;
  };
-
+// making function for confirmation if the user wants capital letters, returns boolean value as the new value of variable.
  function  uppercasePrompt(){
    uppercase = confirm("include uppercase letters?");
    if (uppercase===true){
@@ -39,7 +38,7 @@ var specialCharacters = undefined;
   };
   return;
  };
-
+// making function for confirmation if the user wants numbers, returns boolean value as the new value of variable.
  function numericPrompt(){
    numeric = confirm("include numeric characters?");
    if (numeric===true){
@@ -49,7 +48,7 @@ var specialCharacters = undefined;
   };
   return;
  };
-
+// making function for confirmation if the user wants special characters, returns boolean value as the new value of variable.
  function specialPrompt(){
    specialCharacters = confirm("include special characters? (!@$#%^&*...etc)");
    if (specialCharacters===true){
@@ -59,7 +58,7 @@ var specialCharacters = undefined;
   };
   return;
  };
-
+//prompts user with an alert that tells them they have finished inputing data for the password generator.
  function completionPrompt(){
    alert("thank you for your input(s)");
 
@@ -67,13 +66,14 @@ var specialCharacters = undefined;
  };
 
  function generatePassword() {
+   //declaring arrays to coorespond with the choices from the prompts
   var upperLetterArray = ["Q","W","E","R","T","Y","U","I","O","P","A","S","D","F","G","H","J","K","L","Z","X","C","V","B","N","M"];
   var lowerLetterArray = ["q","w","e","r","t","y","u","i","o","p","a","s","d","f","g","h","j","k","l","z","x","c","v","b","n","m"];
   var numericArray = ["1","2","3","4","5","6","7","8","9"];
   var specialArray = ["!","@","#","$","%","^","&","*","(",")","_","-","+","=",";",":","?",">","<",",","."];
   var masterArray = []
   var childArray = []
-
+//combines applicable arrays to be put together into a master array
   if (numeric===true){
     masterArray=masterArray.concat(numericArray);
   };
@@ -86,9 +86,11 @@ var specialCharacters = undefined;
   if(specialCharacters===true){
     masterArray=masterArray.concat(specialArray);
   };
+  // this for loop adds a random index of the master array to the child array, to make an array the length of the desired password character length
   for (let i = 0; i < passwordLength; i++) {
     childArray.push(masterArray[Math.floor(Math.random()*masterArray.length)]);
   };
+  // causes the function to return all elements of the generated child array as a string, with no spaces between the characters.
   return childArray.join("");
  };
 
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Password-Generator-isaacSevering-js
 modifying starter code to create an application that enables users to generate random passwords based on criteria that they’ve selected. This app will run in the browser and will feature dynamically updated HTML and CSS powered by JavaScript code that I write.
+
+# How to run this project
+This project is deployed live at: (https://sevvy77.github.io/Password-Generator-isaacSevering-js/).
+
+You can see the repository that serves this application at: (https://github.com/Sevvy77/Password-Generator-isaacSevering-js).
+
+## contributors
+just me, Isaac Severing.
